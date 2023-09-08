99d0d191571c7415c62dd5ae841ace2f25b4f52e
LauraRossi229 <122574291+LauraRossi229@users.noreply.github.com>
2023-09-08 20:37:34 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 8080;
 
-mongoose.connect('mongodb+srv://rossil229:QPwoer5512122l@cluster0.oeqfqws.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://rossil229:xxxxxxx@cluster0.oeqfqws.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
